6603ea00ea72c3c9a0a55bad590ad55d5b836538
Ethan <ethanalderson777@gmail.com>
2023-01-15 21:27:44 -0500

    undid argparsing and began detection overhauling

--- detect_aruco_video.py
+++ detect_aruco_video.py
@@ -40,46 +40,29 @@ def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
     return resized
 
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-t", "--type", type=str,
-    default="DICT_ARUCO_ORIGINAL",
-    help="type of ArUCo tag to detect")
-args = vars(ap.parse_args())
+# # construct the argument parser and parse the arguments
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-t", "--type", type=str,
+#     default="DICT_ARUCO_ORIGINAL",
+#     help="type of ArUCo tag to detect")
+# args = vars(ap.parse_args())
+
+
 # define names of each possible ArUco tag OpenCV supports
-ARUCO_DICT = {
-    "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
-    "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
-    "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
-    "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
-    "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
-    "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
-    "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
-    "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
-    "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
-    "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
-    "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
-    "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
-    "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
-    "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
-    "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
-    "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
-    "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
-    "DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
-    "DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
-    "DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
-    "DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
-}
+# ARUCO_DICT = {
+#     "DICT_4X4_50": cv2.aruco.DICT_4X4_50
+# }
 # verify that the supplied ArUCo tag exists and is supported by
 # OpenCV
-if ARUCO_DICT.get(args["type"], None) is None:
-    print("[INFO] ArUCo tag of '{}' is not supported".format(
-        args["type"]))
-    sys.exit(0)
+# if ARUCO_DICT.get(args["type"], None) is None:
+#     print("[INFO] ArUCo tag of '{}' is not supported".format(
+#         args["type"]))
+#     sys.exit(0)
+
 # load the ArUCo dictionary and grab the ArUCo parameters
-print("[INFO] detecting '{}' tags...".format(args["type"]))
-arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[args["type"]])
-arucoParams = cv2.aruco.DetectorParameters_create()
+print("[INFO] detecting 4X4_50 tags...")
+arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+arucoParams = cv2.aruco.DetectorParameters()
 # initialize the video stream and allow the camera sensor to warm up
 print("[INFO] starting video stream...")
 
@@ -102,8 +85,10 @@ while(True):
 
     #ids = np.array([])
     # detect ArUco markers in the input frame
-    (corners, ids, rejected) = cv2.aruco.detectMarkers(frame,
-        arucoDict, parameters=arucoParams)
+
+    detector = cv2.aruco.ArucoDetector(arucoDict, arucoParams)
+
+    (corners, ids, rejected) = detector.detectMarkers(frame)
 
 
     rvecs = np.array([])
